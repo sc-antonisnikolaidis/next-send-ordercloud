3ababff4c0c05f96c7983f630bde0d10d35d3be7
Antonis Nikolaidis <antn@sitecore.net>
2022-11-15 23:37:14 +0200

    Add moosend script in app component

--- site/pages/_app.tsx
+++ site/pages/_app.tsx
@@ -6,6 +6,7 @@ import { FC, ReactNode, useEffect } from 'react'
 import type { AppProps } from 'next/app'
 import { Head } from '@components/common'
 import { ManagedUIContext } from '@components/ui/context'
+import Script from 'next/script'
 
 const Noop: FC<{ children?: ReactNode }> = ({ children }) => <>{children}</>
 
@@ -19,6 +20,10 @@ export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head />
+
+      {/* LOAD SITECORE SEND TRACKER from public folder (load once for all pages) */}
+      <Script src="/scripts/mootrack.js" />
+
       <ManagedUIContext>
         <Layout pageProps={pageProps}>
           <Component {...pageProps} />
