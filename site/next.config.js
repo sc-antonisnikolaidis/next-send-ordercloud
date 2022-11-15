fadd03b8d574dcaef147eb6cc82ee1e8ec0f6a61
Antonis Nikolaidis <antn@sitecore.net>
2022-11-15 23:28:01 +0200

    Add moosend tracking

--- site/next.config.js
+++ site/next.config.js
@@ -39,6 +39,7 @@ module.exports = withCommerceConfig({
   // Avoid Module not found: ESM packages (supports-color) need to be imported. Use 'import' to reference the package instead. https://nextjs.org/docs/messages/import-esm-externals
   experimental: {
     esmExternals: 'loose',
+    forceSwcTransforms: true,
   },
 })
 
